1c4c7db988e43f3248b76613ffe9fb48822b7ec2
mohfadel <mohfadel2016@gmail.com>
2023-11-01 22:26:36 +0100

    added foto

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
     description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self) -> str:
         return self.name
